9904e0df5aaa16f8a4cbde3f23aa8a928e0c9bcd
Sung Kim <sungmkim80@gmail.com>
2018-07-07 18:28:14 -0400

    Cleaned some codes & removed unused lodash packages

--- src/components/MouseTracker.js
+++ src/components/MouseTracker.js
@@ -1,6 +1,4 @@
 import React from "react";
-// import debounce from "lodash.debounce";
-import throttle from "lodash.throttle";
 import styled, { keyframes } from "styled-components";
 
 const rotate360 = keyframes`
@@ -13,18 +11,12 @@ const rotate360 = keyframes`
     }
 `;
 
-// const StyledCatImage = styled.img.attrs({
-//   src: "/cat.jpg",
-//   alt: "cat!"
-// })`
-//   display: inline-block;
-//   /* position: absolute; */
-//   animation: 1s ${rotate360} sine infinite;
-//   padding: 2rem 1rem;
-//   size: 1.5rem;
-// `;
-
 const CatImage = () => <img src="/cat.jpg" alt="cat!" width="150px" />;
+
+// ⚠ !!! WARNING!!!
+// Add the props using `attrs` or else it'd generate 200+ classes!!!
+
+// `.attrs` - https://www.styled-components.com/docs/api#attrs
 const CatImageContainer = styled.div.attrs({
   style: props => ({
     top: props.top,
@@ -38,7 +30,6 @@ const CatImageContainer = styled.div.attrs({
 class Cat extends React.Component {
   render() {
     const { x, y } = this.props.mouse;
-    // console.log(`{${x}, ${y}}`);
 
     return (
       <CatImageContainer top={y} left={x}>
@@ -64,30 +55,19 @@ class Mouse extends React.Component {
 
   render() {
     return (
-      <div
-        style={{ height: "100%" }}
-        onMouseMove={this.handleMouseMove}
-        // onMouseMove={e => throttle(this.handleMouseMove(e), 500)}
-      >
+      <div style={{ height: "100%" }} onMouseMove={this.handleMouseMove}>
+        {/* https://reactjs.org/docs/render-props.html */}
         {this.props.render(this.state)}
       </div>
     );
   }
 }
-// Here we create a component that will rotate everything we pass in over two seconds
-// const Rotate = styled.div`
-//   display: inline-block;
-//   animation: ${rotate360} 1s cubic-bezier(0.68, -0.55, 0.27, 1.55) infinite;
-//   padding: 2rem 1rem;
-//   font-size: 1.2rem;
-// `;
 
 class MouseTracker extends React.Component {
   render() {
     return (
       <div id="tracker" style={{ height: "500px" }}>
         <h1>Move the mouse around!</h1>
-        {/* <Rotate>&lt; 💅 &gt;</Rotate> */}
         <Mouse render={mouse => <Cat mouse={mouse} />} />
       </div>
     );
